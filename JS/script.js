400379baf072a3d86788ba100989af9e52d205b9
EmmaH005 <emma.hemphill@edu.sait.ca>
2024-11-12 14:37:35 -0700

    add.year.and.button.docx
    add dynamic year and greeting in JS

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,25 @@
+
+// Dynamic Greeting Button
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+    greeting();
+    console.log("button clicked");
+});
+
+const greeting = () => {
+    if (d.getHours() <= 11) {
+        alert(`Good morning!`);
+    } else if (d.getHours() < 18) {
+        alert(`Good afternoon!`);
+    } else {
+        alert(`Good evening!`);
+    }
+}
+
 // dynamic year
 const fYear = document.getElementById("footerYear");
 const d = new Date();
 fYear.innerText = d.getFullYear();
-// fYear.textContent = d;
+
 console.log(d);
+
