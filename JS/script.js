5de4ef42ac51eb7afdbce0bb0a969529f7b73dc4
EmmaH005 <emma.hemphill@edu.sait.ca>
2024-11-14 10:40:06 -0700

    Update.background.docx
    Change background

--- JS/script.js
+++ JS/script.js
@@ -28,15 +28,3 @@ fYear.innerText = d.getFullYear();
 console.log(d);
 
 
-const body = document.querySelector('body');
-const date = new Date();
-const hour = date.getHours();
-
-if (hour < 12) {
-  body.style.backgroundColor = 'lightblue';
-} else if (hour < 18) {
-  body.style.backgroundColor = 'orange';
-} else {
-  body.style.backgroundColor = 'black';
-}
-
