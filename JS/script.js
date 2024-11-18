8dcf1e223336a2ba95a065d5f6ea6eed3c47dc17
EmmaH005 <emma.hemphill@edu.sait.ca>
2024-11-17 23:03:24 -0700

    add.list.docx
    Add list in JS

--- JS/script.js
+++ JS/script.js
@@ -27,13 +27,24 @@ const greeting = () => {
     }
 }
 
-
-   
-
-
 // dynamic year
 const fYear = document.getElementById("footerYear");
 const d = new Date(2024);
-fYear.innerText = d.getFullYear();
+
 
 console.log(d); 
+
+// list: Couldn't figure out how to populate list
+
+for (let i =1; i<12, i++;) {
+    const li=
+    document.createElement("li");
+    if (i % 2 ===0) {
+ li.textContent = "even";
+    }
+    else{
+ li.textContent = "odd"
+    }
+
+    
+    document.getElementById('number').appendChild}
